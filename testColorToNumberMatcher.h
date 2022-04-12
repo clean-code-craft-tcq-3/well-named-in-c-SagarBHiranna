1660c74785f8489e98953367de76e1b63d14f238
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-04-12 17:34:08 +0530

    Update testColorToNumberMatcher.h

--- testColorToNumberMatcher.h
+++ testColorToNumberMatcher.h
@@ -5,12 +5,12 @@
 
 
 void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor);
+    Major_Colour expectedMajor,
+    Minor_Colour expectedMinor);
     
 void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
+    Major_Colour major,
+    Minor_Colour minor,
     int expectedPairNumber);
 
 #endif
